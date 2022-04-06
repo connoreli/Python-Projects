f351b7b6ebf1daa4315d75ed2ef063af36c541ba
Connor Eliason <connor.eliason@gmail.com>
2022-04-06 14:30:55 -0600

    Update classes.py

--- classes.py
+++ classes.py
@@ -1,15 +1,15 @@
 
-class Vehicle:
+class Vehicle:        # parent class
     make = 'Toyota'
     model = 'Camry'
     year = 2014
     VIN = 93819321
 
-class Sales(Vehicle):
+class Sales(Vehicle):   # child class no.1
     price = '$12,699'
     salesperson = 'John Jackson'
 
-class Detailing(Vehicle):
+class Detailing(Vehicle):  # child class no.2
     Inside_Detailed = True
     Outside_Detailed = True
     Detailing_Company = 'Swiffy Detailing'
