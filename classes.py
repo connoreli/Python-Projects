b3b75186419dc5220e9a6e4edb0930a08606c6d4
Connor Eliason <connor.eliason@gmail.com>
2022-04-06 14:29:50 -0600

    Create classes.py

---
+++ classes.py
@@ -0,0 +1,15 @@
+
+class Vehicle:
+    make = 'Toyota'
+    model = 'Camry'
+    year = 2014
+    VIN = 93819321
+
+class Sales(Vehicle):
+    price = '$12,699'
+    salesperson = 'John Jackson'
+
+class Detailing(Vehicle):
+    Inside_Detailed = True
+    Outside_Detailed = True
+    Detailing_Company = 'Swiffy Detailing'
