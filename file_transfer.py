1e3d4836c1f6b0b0f95cdf0ece39683f4f39c700
Connor Eliason <connor.eliason@gmail.com>
2022-04-15 15:28:41 -0600

    .

--- file_transfer.py
+++ file_transfer.py
@@ -11,4 +11,29 @@ files = os.listdir(source)
 for i in files:
     #we are saying move the files represnted by 'i' to their new destination
     shutil.move(source+i, destination)
-    
+
+
+
+class file(Frame):
+    def __init__(self):
+        tk.Frame.__init__(self)
+        self.pack()
+        self.master.title("File Transfer")
+        self.button1 = Button(self, text = "BROWSE", width = 25,
+                               command = self.new_window )
+        self.button1.grid( row = 0, column = 1, columnspan = 2, sticky = W+E+N+S )
+    def new_window(self):
+        self.newWindow = file2()
+class file2(Frame):     
+    def __init__(self):
+        new =tk.Frame.__init__(self)
+        new = Toplevel(self)
+        new.button = tk.Button(  text = "PRESS TO CLOSE", width = 25,
+                                 command = self.close_window )
+        new.button.pack()
+    def close_window(self):
+        self.destroy()
+def main(): 
+    file().mainloop()
+if __name__ == '__main__':
+    main()
--- web_page_generator.py
+++ web_page_generator.py
@@ -1,3 +1,6 @@
+from tkinter import *
+import tkinter as tk
+
 text = '''
 <html>
     <body>
@@ -10,3 +13,29 @@ file = open("web_page_generator.html","w")
 file.write(text)
 file.close()
 
+class ParentWindow(Frame):
+    def __init__ (self, master):
+        Frame.__init__ (self)
+
+        self.master = master
+        self.master.resizable(width=False, height=False)
+        self.master.geometry('{}x{}'.format(700,400))
+        self.master.config(bg='lightgray')
+
+        self.varBody = StringVar()
+
+    def load_gui(self):
+        self.txt_body = tk.Entry(self.master, text='')
+        self.txt_body.grid(row=1, column=0, rowspan=1, columnspan=2, padx=(30,40), pady=(0,0), sticky=N+E+W)
+
+        self.btnSubmit = Button(self.master, text='Submit', width=10, height=2, command=self.submit)
+        self.btnSubmit.grid(row=2, column=1, padx=(0,0), pady=(30,0), sticky=NE)
+
+    def submit(self):
+        
+        
+
+
+
+if __name__ == '__main__':
+    pass
