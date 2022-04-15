985b0fc9ef6f0b88e44146ee90c2de472eaf56bb
Connor Eliason <connor.eliason@gmail.com>
2022-04-15 13:03:06 -0600

    Update web_page_generator.py

--- web_page_generator.py
+++ web_page_generator.py
@@ -1,8 +1,12 @@
+text = '''
+<html>
+    <body>
+        <h1>Stay tuned for a summer sale!</h1>
+    </body>
+</html>
+'''
 
+file = open("web_page_generator.html","w")
+file.write(text)
+file.close()
 
-f = open('myfile.txt', 'w')
-f.write('Stay tuned for our amazing summer sale!')
-f.close()
-
-f = open('myfile.txt', 'r')
-print(f.read())
