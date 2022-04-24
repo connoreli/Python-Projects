38892d24519d0aed6075fa8ba9d377e88f68173a
Connor Eliason <connor.eliason@gmail.com>
2022-04-24 16:02:01 -0600

    .

--- sqlite.py
+++ sqlite.py
@@ -1,12 +1,7 @@
+
+
 import sqlite3
 
-# get personal data from user
-firstName = input('Enter your first name: ')
-lastName = input('Enter your last name: ')
-age = int(input('Enter your age:'))
-
-# execute insert statement for supplied person data
-with sqlite3.connect('test_database.db') as connection:
-    c = connection.cursor()
-    line = "INSERT INTO People VALUES ('"+ firstName +"', '"+ lastName+"', " + str(age) +")"
-c.execute(line)    
+with sqlite3.connect('Roster.db') as conn:
+    c = conn.cursor()
+    roster = ("""CREATE TABLE IF NOT EXISTS tbl_roster""")
